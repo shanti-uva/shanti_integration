3aa3fb2927842ca27562318b51d37ca5dacfc9bc
Andres Montano <amontano@virginia.edu>
2013-05-17 20:58:22 -0600

    fixed error with cache

--- lib/shanti_integration/site.rb
+++ lib/shanti_integration/site.rb
@@ -1,7 +1,7 @@
 module InterfaceUtils
   module Server
     def self.get_url
-      Rails.cache.fetch('server/environment') do
+      Rails.cache.fetch('server/domain') do
         case environment
         when DEVELOPMENT then 'http://dev.kmaps.virginia.edu'
         when STAGING     then 'http://staging.kmaps.virginia.edu'
