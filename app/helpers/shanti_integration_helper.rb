b5443c7b6dc8a6d6c0378d677648873c0359c3ce
Andres Montano <amontano@virginia.edu>
2013-05-17 21:44:29 -0600

    adding google api key

--- app/helpers/shanti_integration_helper.rb
+++ app/helpers/shanti_integration_helper.rb
@@ -1,5 +1,8 @@
 # Methods added to this helper will be available to all templates in the application.
 module ShantiIntegrationHelper
+  def google_maps_key
+    'AIzaSyAXpnXkPS39-Bo5ovHQWvyIk6eMgcvc1q4'
+  end
   
   def stylesheet_files
     ['application']
