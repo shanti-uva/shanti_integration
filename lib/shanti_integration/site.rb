dff0d830b568af8a137dde02f510a886b1b3a2cd
Andres Montano <amontano@virginia.edu>
2014-04-22 20:16:08 -0600

    update cache expiration

--- lib/shanti_integration/site.rb
+++ lib/shanti_integration/site.rb
@@ -1,7 +1,7 @@
 module InterfaceUtils
   module Server
     def self.get_url
-      Rails.cache.fetch('server/domain') do
+      Rails.cache.fetch('server/domain', :expires_in => 1.day) do
         case environment
         when DEVELOPMENT then 'http://dev.thlib.org'
         when STAGING     then 'http://staging.thlib.org'
--- lib/shanti_integration/version.rb
+++ lib/shanti_integration/version.rb
@@ -1,3 +1,3 @@
 module ShantiIntegration
-  VERSION = '2.3.0'
+  VERSION = '2.3.1'
 end
