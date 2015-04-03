6a1346b82c71c96d10b2a4fc3cb6573f16910f22
Andres Montano <amontano@virginia.edu>
2015-04-03 11:44:07 -0600

    adding fonts precompiled assets

--- lib/shanti_integration/engine.rb
+++ lib/shanti_integration/engine.rb
@@ -1,4 +1,8 @@
 module ShantiIntegration
   class Engine < ::Rails::Engine
+    initializer :assets do |config|
+      Rails.application.config.assets.precompile.concat(['shanticon/shanticon.eot', 'shanticon/shanticon.svg',
+        'shanticon/shanticon.ttf', 'shanticon/shanticon.woff'])
+    end
   end
 end
--- lib/shanti_integration/version.rb
+++ lib/shanti_integration/version.rb
@@ -1,3 +1,3 @@
 module ShantiIntegration
-  VERSION = '3.1.0'
+  VERSION = '3.1.1'
 end
